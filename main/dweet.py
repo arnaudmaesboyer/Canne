90c106d6f3cf35d141b6ca3f5a93e26ee97fa2fc
arnaudmaesboyer <43748615+arnaudmaesboyer@users.noreply.github.com>
2018-11-07 21:46:37 +0100

    Update dweet.py

--- main/dweet.py
+++ main/dweet.py
@@ -5,7 +5,10 @@ def Compteur {
 def envoi {
   #Fonction gérant l'envoi des données récolté a dweet et ajoutant les données si plusieurs données envoyées le meme jour
 }
+def reception {
+  #Fonction gérant la reception des données de dweet
+}
 
-def connection {
+def connexion {
   #Gestion de la connexion au wifi du portable parametrée d'avance . 
 }
