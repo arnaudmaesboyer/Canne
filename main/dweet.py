9b1fbb9d10a2d9dc76495bf431cb1fdc6aed1a0f
arnaudmaesboyer <43748615+arnaudmaesboyer@users.noreply.github.com>
2018-11-07 21:40:19 +0100

    Update dweet.py

--- main/dweet.py
+++ main/dweet.py
@@ -1,1 +1,11 @@
+def Compteur {
+  #Fonction permettant de stocker dans une variable le nombre de mur rencontré
+}
 
+def envoi {
+  #Fonction gérant l'envoi des données récolté a dweet et ajoutant les données si plusieurs données envoyées le meme jour
+}
+
+def connection {
+  #Gestion de la connexion au wifi du portable parametrée d'avance. 
+}
