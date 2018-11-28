f793b749cc760b0c00e6d81c367e2c288075e88c
arnaudmaesboyer <43748615+arnaudmaesboyer@users.noreply.github.com>
2018-11-28 11:55:37 +0100

    2 Capteur ultra son + radar de recul

--- main/canne.py
+++ main/canne.py
@@ -1,16 +1,61 @@
+# coding: utf-8
 import time
 from grovepi import *
 
 # Connect the Grove Buzzer to digital port D8
 # SIG,NC,VCC,GND
-buzzer = 8
+buzzer = 2
 ultrasonic_ranger1 = 4
-ultrasonic_ranger2 = 5 ### A VERIFIER
-ultrasonic_ranger3 = 6 ### A VERIFIER
-button = 3
+ultrasonic_ranger2 = 3 
+button = 7
 
 pinMode(button,"INPUT")
 pinMode(buzzer,"OUTPUT")
+
+compteurMarche = 0 #Appel à la fonction qui renvoie le nombre de marches rencontrées ce jour-ci grâce à une requete de à dweet
+booleanMarche = False
+
+def activationBuzzer(tempsBuzzerInactif):
+     digitalWrite(buzzer,1)
+     time.sleep(0.1)
+     digitalWrite(buzzer,0)
+     time.sleep(tempsBuzzerInactif)
+
+def frequenceBuzzer(valeurCapteur,booleanMarche):
+    if (valeurCapteur<5):
+         activationBuzzer(0.1)
+         print ("Capteur 1 activé")
+         return False
+     
+
+    elif (valeurCapteur<15):
+         activationBuzzer(0.2)
+         return False
+
+    elif (valeurCapteur<25):
+         activationBuzzer(0.3)
+         return False
+
+    elif (valeurCapteur<35):
+         activationBuzzer(0.4)
+         return False
+
+    elif (valeurCapteur<45):
+         activationBuzzer(0.5)
+         return False
+
+    elif (valeurCapteur<50):
+         activationBuzzer(0.6)
+         return False
+
+    else :
+             
+	digitalWrite(buzzer,0)
+	compteur = 0
+	print ("Buzzer désactivé")
+	if(booleanMarche == False):
+             return True
+
 
 while True:
     
@@ -20,66 +65,30 @@ while True:
     
     # Si le boutton On/Off est activé alors les capteurs ultrasons sont lancés
     while digitalRead(button)==1:
-        try:
+	print ("Bouton appuyé")
+     #   try:
             # Buzz si le capteur 1 est activé
-            if (ultrasonicRead(ultrasonic_ranger1)<2):
-                digitalWrite(buzzer,1)
-                print ultrasonicRead(ultrasonic_ranger1)
-                print ('start')
-        
+	valeurCapteur1 = ultrasonicRead(ultrasonic_ranger1)
+	valeurCapteur2 = ultrasonicRead(ultrasonic_ranger2)
+	
+	print ("Distance Capteur 1")
+	print (valeurCapteur1)
 
-            # Stop le buzz du capteur 1
-            if(ultrasonicRead(ultrasonic_ranger1)>=2):
-                digitalWrite(buzzer,0)
-                print ultrasonicRead(ultrasonic_ranger1)
-                print ('stop')
+	print ("Distance Capteur 2")
+	print (valeurCapteur2)
 
-
-
-            #########----------------###########
-
+        booleanMarche = frequenceBuzzer(valeurCapteur1,booleanMarche)
+        if(booleanMarche):
+             compteurMarche = compteurMarche + 1
+             
+        frequenceBuzzer(valeurCapteur2,booleanMarche)
 
 
-            # Buzz si le capteur 2 est activé
-            if (ultrasonicRead(ultrasonic_ranger2)<2):
-                digitalWrite(buzzer,1)
-                print ultrasonicRead(ultrasonic_ranger2)
-                print ('start')
-        
-
-            # Stop le buzz du capteur 2
-            if(ultrasonicRead(ultrasonic_ranger2)>=2):
-                digitalWrite(buzzer,0)
-                print ultrasonicRead(ultrasonic_ranger2)
-                print ('stop')
-
-
-
-            #########----------------###########
-
-
-
-            # Buzz si le capteur 3 est activé
-            if (ultrasonicRead(ultrasonic_ranger3)<2):
-                digitalWrite(buzzer,1)
-                print ultrasonicRead(ultrasonic_ranger3)
-                print ('start')
-        
-
-            # Stop le buzz du capteur 3
-            if(ultrasonicRead(ultrasonic_ranger3)>=2):
-                digitalWrite(buzzer,0)
-                print ultrasonicRead(ultrasonic_ranger3)
-                print ('stop')
-
-
-
-        except KeyboardInterrupt:
-            digitalWrite(buzzer,0)
-            break
-        except IOError:
-            print ("Error")
-            
+    if(booleanMarche == True):
+         compteurMarche = compteurMarche + 1
+    print ("Bouton relaché")
+    print("CompteurMarche :")
+    print(compteurMarche)
             
 ### def CanneEstTombe() ### fonction permettant d'émettre un bip d'une certaine fréquence pour indiquer un changement brutal
 ### dans les valeurs de l'accéléromètre qui indiquerais une chute de la canne
